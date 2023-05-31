1d3e52055311c6c0b2d06c846bf581fa70087825
lautarocozzi <clautaro@outlook.com>
2023-05-31 20:17:28 -0300

    Terminando parcial

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,41 @@ import './App.css'
 import Card from './Components/Card'
 
 function App() {
+
+  const [nombre, setNombre] = useState("")
+  const [pelicula, setPelicula] = useState("")
+  const [error, setError] = useState(false)
+  const [show, setShow] = useState(false)
   
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(nombre.length > 2 && pelicula.length > 6){
+      setError(false)
+      setShow(true)
+    }else{
+      setError(true)
+      setShow(false)
+    }
+
+
+  }
 
   return (
     <div className='App'>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card/>      
+      <h1>Parcial React</h1>
+      <form className='form' onSubmit={handleSubmit}>
+        
+        <input placeholder='Ingrese su nombre' type='text' value={nombre} onChange={(e) => { setShow(false); return setNombre(e.target.value.trim())}}></input>
+        
+        <input placeholder='Ingrese su Pelicula favorita' type='text' value={pelicula} onChange={(e) => { setShow(false); return setPelicula(e.target.value)}}></input>
+
+        <button>Enviar!</button>
+        <div className='error'>
+        {error && "Por favor chequea que la información sea correcta" }
+        </div>
+      </form>
+      {show ? <Card  disabled={show} nombre={nombre} pelicula={pelicula}/> : null}      
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Card.css'
 
-const Card = () => {
+
+const Card = ({nombre,pelicula}) => {
   return (
-    <div>Card</div>
+    <div className='card'>
+        <h3>Hola {nombre}</h3>
+        <h4>Sabemos que tu pelicula favorita es:</h4>
+        <div className='pelicula'>{pelicula}</div>
+
+    </div>
   )
 }
 
